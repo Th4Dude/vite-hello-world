fbb9a3ffcf361b5df8c46c251c4c6e254ff0795b
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-22 18:41:31 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
-
+  import AppTitle from './components/AppTitle.vue'; 
+  export default{
+      components:{
+        AppTitle
+      }
+  }
 </script>
 
 <template>
-  
+  <AppTitle></AppTitle>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+    export default {
+        data(){
+            return{
+                message : 'la mia prima app con vite',
+            }
+        }
+    }
+
+
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+
+
+
+
+</style>
